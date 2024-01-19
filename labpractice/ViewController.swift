be8e0b33c750e356f7f53cd2ed9f80d3d4402ab9
yjpatel0 <yash91200@gmail.com>
2024-01-19 16:16:27 -0500

    Initial Commit

---
+++ labpractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  labpractice
+//
+//  Created by user237705 on 1/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
