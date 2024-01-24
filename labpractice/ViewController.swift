115f4fa1d2b7f07478c10b4cfe18c98f0fa14e6c
yjpatel0 <yash91200@gmail.com>
2024-01-23 19:52:07 -0500

    Designed

--- labpractice/ViewController.swift
+++ labpractice/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var name: UILabel!
+    
+    var tempNumberHolder = 0
+
+    @IBAction func nimusbutton(_ sender: Any) {
+        tempNumberHolder -= 1
+        name.text = String(tempNumberHolder)
+    }
+    
+
+    @IBAction func plusebutton(_ sender: Any) {
+        tempNumberHolder += 1
+        name.text = String(tempNumberHolder)
+    }
+    
+
+    @IBAction func doubleplus(_ sender: Any) {
+        tempNumberHolder += 2
+        name.text = String(tempNumberHolder)
+    }
+    
+    @IBAction func doubleminus(_ sender: Any) {
+        tempNumberHolder -= 2
+        name.text = String(tempNumberHolder)
+    }
+    @IBAction func restButton(_ sender: Any) {
+        tempNumberHolder = 0
+        name.text = String(tempNumberHolder)
+    }
+    
+    
+    
+    @IBAction func addTwo(_ sender: Any) {
+        if (tempNumberHolder > 0)
+        {
+            tempNumberHolder += 2
+            name.text = String(tempNumberHolder)
+        
+        }
+        else if (tempNumberHolder < 0)
+        {
+            tempNumberHolder -= 2
+            name.text = String(tempNumberHolder)
+        }
+        
+        else
+        {
+            tempNumberHolder += 2
+            name.text = String(tempNumberHolder)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
